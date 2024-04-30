0b9a706b9266a974a6c10fe917a968fa506b287d
Soraia Tavares <soraiatosdeoliveira@hotmail.com>
2024-04-30 09:14:10 -0300

    Commit do dia 30 de abril de 2024

--- app/src/main/java/tavares/soraia/lista/activity/MainActivity.java
+++ app/src/main/java/tavares/soraia/lista/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package tavares.soraia.lista;
+package tavares.soraia.lista.activity;
 
 import android.os.Bundle;
 
@@ -8,17 +8,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class NewItemActivity extends AppCompatActivity {
+import tavares.soraia.lista.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_new_item);
+        setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
--- app/src/main/java/tavares/soraia/lista/activity/NewItemActivity.java
+++ app/src/main/java/tavares/soraia/lista/activity/NewItemActivity.java
@@ -1,4 +1,4 @@
-package tavares.soraia.lista;
+package tavares.soraia.lista.activity;
 
 import android.os.Bundle;
 
@@ -8,18 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import tavares.soraia.lista.R;
+
+public class NewItemActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_new_item);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
     }
 }
